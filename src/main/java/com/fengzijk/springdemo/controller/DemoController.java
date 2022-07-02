a2002a0d37fc398e2e76dc76195a6d05ec6ea851
fengzijk <guozhifengvip@gmail.com>
2022-07-03 00:38:18 +0800

    时间转换

--- src/main/java/com/fengzijk/springdemo/controller/DemoController.java
+++ src/main/java/com/fengzijk/springdemo/controller/DemoController.java
@@ -23,9 +23,17 @@ import com.fengzijk.springdemo.entity.DemoEntity;
 import com.fengzijk.springdemo.mapper.DemoMapper;
 import com.fengzijk.springdemo.service.DemoService;
 import com.fengzijk.springdemo.util.JsonUtil;
+import java.beans.PropertyEditorSupport;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -51,5 +59,35 @@ public class DemoController {
         return new ResponseEntity<String>().ok().setdata("success");
     }
 
+    @InitBinder
+    public void initBinder(final WebDataBinder webdataBinder) {
+        webdataBinder.registerCustomEditor(LocalDateTime.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                long remindTime = Long.parseLong(text);
+                setValue(LocalDateTime.from(Instant.ofEpochMilli(remindTime).atZone(ZoneId.systemDefault())));
+            }
+        });
+
+        webdataBinder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(new Date(Long.parseLong(text)));
+            }
+        });
+    }
+
+
+    @GetMapping("/get/{time}")
+    public ResponseEntity<String> test1(@PathVariable(value = "time") LocalDateTime  time ) {
+
+        log.info("time:{}",time);
+
+        demoService.selectTest();
+        //DemoEntity listEntity2 = demoMapper.selectByxxId(25L);
+        //log.info("listEntity2================:{}", JsonUtil.tojson(listEntity2));
+
+        return new ResponseEntity<String>().ok().setdata("success");
+    }
 
 }
